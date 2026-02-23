7c68cb36c50dd0a165265d414f984814ae565988
Tshego050611 <4460481@myuwc.ac.za>
2026-02-23 14:55:59 +0200

    Update 13template.java

--- 13template.java
+++ 13template.java
@@ -1,1 +1,10 @@
-// Comment 1 
+import java.lang.Math.*; import java.io.*; import java.text.*;
+public class timeMethods{
+  public static int N = 32654;
+  public static void main(String args []){
+
+    DecimalFormat twoD = new DecimalFormat("0.00");
+    DecimalFormat fourD = new DecimalFormat("0.0000");
+    DecimalFormat fiveD = new DecimalFormat("0.00000);
+
+    
